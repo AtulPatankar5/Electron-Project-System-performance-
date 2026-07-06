657fbf577f8e6dc12fa6e50a9abecc3d33220239
Atul Patankar <atul.patankar@fortytwolabs.com>
2026-07-06 14:34:52 +0530

    Working app completed : Application was created

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -35,7 +35,6 @@ function App() {
       <section id="next-steps">
         <div id="docs">
           <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
           </svg>
           <h2>Documentation</h2>
           <p>Your questions, answered</p>
@@ -56,7 +55,6 @@ function App() {
         </div>
         <div id="social">
           <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
           </svg>
           <h2>Connect with us</h2>
           <p>Join the Vite community</p>
@@ -68,7 +66,6 @@ function App() {
                   role="presentation"
                   aria-hidden="true"
                 >
-                  <use href="/icons.svg#github-icon"></use>
                 </svg>
                 GitHub
               </a>
@@ -80,7 +77,6 @@ function App() {
                   role="presentation"
                   aria-hidden="true"
                 >
-                  <use href="/icons.svg#discord-icon"></use>
                 </svg>
                 Discord
               </a>
@@ -92,7 +88,6 @@ function App() {
                   role="presentation"
                   aria-hidden="true"
                 >
-                  <use href="/icons.svg#x-icon"></use>
                 </svg>
                 X.com
               </a>
@@ -104,7 +99,6 @@ function App() {
                   role="presentation"
                   aria-hidden="true"
                 >
-                  <use href="/icons.svg#bluesky-icon"></use>
                 </svg>
                 Bluesky
               </a>
